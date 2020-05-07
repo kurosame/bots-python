59629b659cac2791479db32dcd81fe22c9afbabf
kurosame <kurosame1227@gmail.com>
2020-05-07 16:01:54 +0900

    add: temp_max, but only `16 Day forecast` works

--- bots/weather.py
+++ bots/weather.py
@@ -66,6 +66,7 @@ def main():
         conv_rj["description"] = rj["weather"][0]["description"]
         conv_rj["icon"] = rj["weather"][0]["icon"]
         conv_rj["temp"] = round(rj["main"]["temp"])
+        conv_rj["temp_max"] = round(rj["main"]["temp_max"]) # Only `16 Day forecast` works
         conv_rj["rain"] = round(rj["rain"]["3h"], 1) if "rain" in rj else 0
         arr_rj.append(conv_rj)
 
